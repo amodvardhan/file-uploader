f9d8d0163db6a0e366cc424a5a71b2fb79966ab8
amodvardhan <amod.vardhan002@gmail.com>
2021-03-07 13:59:24 +0530

    Updates

---
+++ precache-manifest.494d76fb30c48b617a8e5849009fa663.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f949e7dddcc841dc9cef8baf262c6d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6297254fad9c7f28dc37",
+    "url": "./static/css/main.5765b1d1.chunk.css"
+  },
+  {
+    "revision": "93633c4d89be9b266b22",
+    "url": "./static/js/2.3ee7673b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.3ee7673b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6297254fad9c7f28dc37",
+    "url": "./static/js/main.30894a01.chunk.js"
+  },
+  {
+    "revision": "652354a5781954033231",
+    "url": "./static/js/runtime-main.b15537f4.js"
+  }
+]);
